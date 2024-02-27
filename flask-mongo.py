b72284bf6fa0087deeb5da1a24d429b34001744b
nardeenNathan123 <nardeennathan@gmail.com>
2024-02-28 01:16:53 +0200

    edit_delete

--- flask-mongo.py
+++ flask-mongo.py
@@ -4,7 +4,7 @@ from flask_pymongo import PyMongo
 app=Flask(__name__,template_folder="test")
 app.config["MONGO_URI"] = "mongodb://localhost:27017/test"
 mongo = PyMongo(app)
-
+users=[]
 @app.route('/users')
 def index():
     users=list(mongo.db.Instractor2.find({}))
@@ -13,10 +13,10 @@ def index():
     else :
         return "<h1> NO Users</h1>"
 def get_next_id():
-   user=list(mongo.db.Instractor2.find({}).sort({"_id":-1}))[0]._id
-   if user==[]:
+   user=list(mongo.db.Instractor2.find({}).sort({"_id":-1}))
+   if user!=[]:
         print("list no empty")
-        return [0]['_id']+1
+        return user[0]['_id']+1
    else :
         print("list empty")
         return 1
@@ -29,7 +29,30 @@ def get_users():
         mongo.db.Instractor2._insert_one({"_id":get_next_id(),'name':name,"age":age,"locaion":location})      
     #print(users)
     
-    return "ok"
+    return redirect('/users')
+
+@app.route('/edit/<int:user_id>')
+def edit_user(user_id):
+    user = mongo.db.Instractor2.find_one({'_id': user_id})
+    if user:
+        return render_template('update_user.html', user=user)
+    else:
+        return "User not found", 404
+    
+@app.route('/update/<int:user_id>', methods=['POST'])
+def update_user(user_id):
+    name = request.form['name']
+    age = request.form['age']
+    location = request.form['location']
+    mongo.db.Instractor2.update_one({'_id': user_id}, {'$set': {'name': name, 'age': age, 'location': location}})
+    return redirect('/users')
+   
+
+@app.route('/delete/<int:user_id>')
+def delete_user(user_id):
+    mongo.db.Instractor2.delete_one({'_id': user_id})
+    return redirect('/users')
+
 
 
 
